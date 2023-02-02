f5425c0e5eb64db866176f372ed8486a7375f764
howard-99 <115505209+howard-99@users.noreply.github.com>
2023-02-02 22:37:35 +0330

    Update ir-mirror.sh

--- ir-mirror.sh
+++ ir-mirror.sh
@@ -1,3 +1,3 @@
 cd /etc/apt/
-wget -O sources.list https://raw.githubusercontent.com/howard-99/ir-mirror/main/sources.list
+wget -O sources.list https://raw.githubusercontent.com/howard-99/ir-mirror/main/aminidc-sources.list
 sudo apt clean
