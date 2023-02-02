4e6ca4e4d844283f41ff9ce116773899abd94174
howard-99 <115505209+howard-99@users.noreply.github.com>
2023-02-02 13:36:53 +0330

    Update ir-mirror.sh

--- ir-mirror.sh
+++ ir-mirror.sh
@@ -1,4 +1,3 @@
-sudo cd /etc/apt/
-wget -O https://raw.githubusercontent.com/howard-99/ir-mirror/main/sources.list
+cd /etc/apt/
+wget -O sources.list https://raw.githubusercontent.com/howard-99/ir-mirror/main/sources.list
 sudo apt clean
-cd
