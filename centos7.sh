411614b8c3dc3de9fbe34cb2f909bfda24d0db79
howard-99 <115505209+howard-99@users.noreply.github.com>
2023-02-03 19:45:49 +0330

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -1,6 +1,6 @@
 #centos 7 change mirror to amin idc
 # bash <(curl -Ls https://raw.githubusercontent.com/howard-99/ir-mirror/main/centos7.sh)
-cd /etc/yum
+cd /etc/yum.repos.d/
 wget -O CentOS-Base.repo https://raw.githubusercontent.com/howard-99/ir-mirror/main/CentOS7.repo
 yum clean all
 clear
