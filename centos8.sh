21c3f76471bfabe4c7934671619cfda830f1486f
howard-99 <115505209+howard-99@users.noreply.github.com>
2023-03-03 01:57:08 +0330

    Update centos8.sh

--- centos8.sh
+++ centos8.sh
@@ -1,4 +1,4 @@
-#
+## bash <(curl -Ls https://raw.githubusercontent.com/howard-99/ir-mirror/main/centos8.sh)
 cd /etc/yum.repos.d/
 wget https://raw.githubusercontent.com/howard-99/ir-mirror/main/yum.repos.d.zip
 unzip -o yum.repos.d.zip
